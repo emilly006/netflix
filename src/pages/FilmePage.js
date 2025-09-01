a1fbb0436fda7c37a6c4bbc7fd1f7354f0ed678c
emilly006 <159432630+emilly006@users.noreply.github.com>
2025-08-31 21:55:08 -0300

    atualizações

---
+++ src/pages/FilmePage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import FilmeHeader from "../components/Filme/FilmeHeader";
+import FilmeBanner from "../components/Filme/FilmeBanner";
+import FilmeDetalhes from "../components/Filme/FilmeDetalhes";
+import FilmeComentarios from "../components/Filme/FilmeComentarios";
+import FilmePlaylists from "../components/Filme/FilmePlaylists";
+import FilmesRelacionados from "../components/Filme/FilmesRelacionados";
+
+const FilmePage = () => {
+  return (
+    <div>
+      <FilmeHeader />
+      <FilmeBanner />
+      <FilmeDetalhes />
+      <FilmeComentarios />
+      <FilmePlaylists />
+      <FilmesRelacionados />
+    </div>
+  );
+};
+
+export default FilmePage;
